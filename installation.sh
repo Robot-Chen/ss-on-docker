d9178e84c342473bec8e832c8b510811a3affe2e
Peachey_Chen <mr_robot2015@foxmail.com>
2019-09-06 00:48:47 +0800

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -2,19 +2,18 @@
 
 set -e
 
-curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh
+# open bbr
+wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh && chmod +x bbr.sh && ./bbr.sh
 
+# install docker 
+curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh
 systemctl start docker
 systemctl enable docker
-
 usermod -aG docker $USER
  
 apt-get install git -y
-
 git clone https://github.com/Robot-Chen/ss-on-docker
-
 cd ss-on-docker
 
 docker build -t peacheychen/ss-on-docker .
-
 docker run -d --name shadowsocks -p 20196:20196 --restart="always" peacheychen/ss-on-docker 
