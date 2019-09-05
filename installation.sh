55709c37fbc7ade83c401a3c47c2a54aa2f56e0a
Peachey_Chen <mr_robot2015@foxmail.com>
2019-09-06 00:14:06 +0800

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+set -e
+
 curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh
 
 systemctl start docker
@@ -12,3 +16,5 @@ git clone https://github.com/Robot-Chen/ss-on-docker
 cd ss-on-docker
 
 docker build -t peacheychen/ss-on-docker .
+
+docker run -d --name shadowsocks -p 20196:20196 --restart="always" peacheychen/ss-on-docker 
