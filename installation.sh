506d526ed7bc6acc0e38f505d9a343db63799061
Peachey_Chen <mr_robot2015@foxmail.com>
2019-09-07 23:28:02 +0800

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -2,14 +2,14 @@
 
 set -ex
 
+# open bbr
+wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh && chmod +x bbr.sh && ./bbr.sh
+
 # install docker 
 curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh
 systemctl start docker
 systemctl enable docker
 usermod -aG docker $USER
- 
-# open bbr
-wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh && chmod +x bbr.sh && ./bbr.sh
 
 apt-get install git -y
 git clone https://github.com/Robot-Chen/ss-on-docker
